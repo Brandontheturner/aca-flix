63d40696ae0bc7d80e091c36b11684ac9d294929
Brandon Turner <brandontheturner@gmail.com>
2019-03-23 15:09:40 -0500

    added nav component

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div>
+      <div id="navigation" className="Navigation">
+        <nav>
+          <ul>
+            <li>Browse</li>
+            <li>My list</li>
+            <li>Top picks</li>
+            <li>Recent</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
