2022b0be23b7ad472d1f06829555bb9ad33f74c4
Brandon Turner <brandontheturner@gmail.com>
2019-03-23 16:27:30 -0500

    added applymiddleware to store function in store

--- src/store.js
+++ src/store.js
@@ -3,6 +3,6 @@ import state from "./state";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(reducers, state);
+const store = createStore(reducers, state, applyMiddleware);
 
 export default store;
