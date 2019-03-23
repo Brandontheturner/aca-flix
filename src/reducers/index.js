80d0b30eb8a7614801a02d4ffd051cf78cef2185
Brandon Turner <brandontheturner@gmail.com>
2019-03-23 15:11:10 -0500

    added imports and components

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import CombineReducers, { combineReducers } from "redux";
+
+function movieSearch(state = [], action) {
+  if (action.type === "MY_MOVIE_LIST_LOADED") {
+    return action.value;
+  }
+  return state;
+}
+function searchResults(state = [], action) {
+  if (action.type === "SEARCH_RESULTS_LOADED") {
+    return action.value;
+  }
+  return state;
+}
+
+const rootReducer = combineReducers({
+  movieSearch,
+  searchResults
+});
+
+export default rootReducer;
