2e8550d1997282f84d38bf91318d58b3311587fc
Brandon Turner <brandontheturner@gmail.com>
2019-03-23 16:23:43 -0500

    added thunk to store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import state from "./state";
 import reducers from "./reducers";
+import thunk from "redux-thunk";
 
 const store = createStore(reducers, state);
 
