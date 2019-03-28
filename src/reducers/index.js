d014b470817b160bda0a3d6b274f0cf23a4dd92b
Brandon Turner <brandontheturner@gmail.com>
2019-03-27 19:17:22 -0500

    adding reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import CombineReducers, { combineReducers } from "redux";
+import { combineReducers } from "redux";
 
 function movieSearch(state = [], action) {
   if (action.type === "MY_MOVIE_LIST_LOADED") {
@@ -6,16 +6,16 @@ function movieSearch(state = [], action) {
   }
   return state;
 }
-// function searchResults(state = [], action) {
-//   if (action.type === "SEARCH_RESULTS_LOADED") {
-//     return action.value;
-//   }
-//   return state;
-// }
+function searchResults(state = [], action) {
+  if (action.type === "SEARCH_RESULTS_LOADED") {
+    return action.value;
+  }
+  return state;
+}
 
 const rootReducer = combineReducers({
-  movieSearch
-  // searchResults
+  movieSearch,
+  searchResults
 });
 
 export default rootReducer;
